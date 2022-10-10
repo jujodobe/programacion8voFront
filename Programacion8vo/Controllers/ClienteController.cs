7eb4489ecfb943425db03f976ef836e46a714934
Juan Dos Santos <jds.infosystem@gmail.com>
2022-10-10 20:14:15 -0300

    big bang

---
+++ Programacion8vo/Controllers/ClienteController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Programacion8vo.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: ClienteController
+        public async Task<ActionResult> Index()
+        {
+            List<Models.clienteModel> clientes = new List<Models.clienteModel>();
+            var api = new HttpClient();
+            var json = await api.GetStringAsync("https://localhost:7279/clientes/6");
+            Models.clienteModel cliente = JsonConvert.DeserializeObject<Models.clienteModel>(json);
+            clientes.Add(cliente);
+            return View(clientes);
+        }
+
+        // GET: ClienteController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClienteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Models.clienteModel cliente)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(cliente);
+                var data = new StringContent(json, Encoding.UTF8, "Application/json");
+
+                var api = new HttpClient();
+
+                var response = await api.PostAsync("https://localhost:7279/clientes/", data);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Programacion8vo/Models/clienteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Programacion8vo.Models
+{
+    public class clienteModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NroDocumeno { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int Edad { get; set; }
+    }
+}
